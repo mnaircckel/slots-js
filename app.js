7afa8bf8fa25bf2f01f71d59cf7eb80651927095
Marcel Champagne <champagn@grinnell.edu>
2016-07-21 21:30:17 -0700

    Add app.js and handlebars

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.listen(3000, function () {
+  console.log('App is listening on port 3000!');
+});
